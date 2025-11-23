39d7942572a1586a48302228ff3e2b52261dace5
Hüseyin <kircihuseyin524@gmail.com>
2025-11-23 20:36:25 +0300

    kapıya sandalye koyulabiliyor

--- Assets/entry_door.cs
+++ Assets/entry_door.cs
@@ -4,20 +4,28 @@ public class entry_door : MonoBehaviour , IInteractable , Iscanlistener
 {
     [SerializeField] bool belled = false;
     [SerializeField] bool barricaded = false;
+    [SerializeField] GameObject ChairObjectToShow;
+    [SerializeField] GameObject BellObjectToShow;
+
     public void OnInteract(Player interactee)
     {
-        if(interactee.Item.GetComponent<door_bell>() != null)
+        interactee.Item.TryGetComponent<door_bell>(out door_bell bell);
+        interactee.Item.TryGetComponent<blockade_chair>(out blockade_chair chair);
+
+        if(bell != null)
         {
             Destroy(interactee.Item.gameObject);
             interactee.Item = null;            
             belled = true;
 
         }
-        else if(interactee.Item.GetComponent<blockade_chair>() != null)
+        else if(chair != null)
         {
             Destroy(interactee.Item.gameObject);
             interactee.Item = null;            
             barricaded = true;
+            ChairObjectToShow.SetActive(true);
+            
         }
     }
 
@@ -29,7 +37,7 @@ public class entry_door : MonoBehaviour , IInteractable , Iscanlistener
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ChairObjectToShow.SetActive(false);
     }
 
     // Update is called once per frame
