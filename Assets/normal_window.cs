d121e5bbcef53e2a3461da0a4a7c390f67e35e34
Hüseyin <kircihuseyin524@gmail.com>
2025-11-26 04:14:31 +0300

    perde ok

--- Assets/normal_window.cs
+++ Assets/normal_window.cs
@@ -2,12 +2,19 @@ using UnityEngine;
 
 public class normal_window : MonoBehaviour , IInteractable , Iscanlistener
 {
-    [SerializeField] float yo = 0;
+    [SerializeField] float counter;
+    const float max = 50;
+    const float min = 5;
     bool holding = false;
+
+    GameObject perde ;
+    Vector3 perdeScale;
+    float startX;
     public void OnInteract(Player interactee)
     {
         holding = true;
-        yo+=Time.deltaTime;
+        if(counter<max)
+            counter+=Time.deltaTime;
         
     }
 
@@ -21,18 +28,26 @@ public class normal_window : MonoBehaviour , IInteractable , Iscanlistener
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        perde = transform.Find("perde").gameObject;
+        startX = perde.transform.localScale.x;
+        perdeScale = transform.localScale;
+        counter = max;
     }
 
     // Update is called once per frame
     void Update()
     {
         //aha buraya azalma şeysi yapılabilir
-        if(holding == false && yo > 0)
-            yo -= Time.deltaTime / 3;
+        if(holding == false && counter > min)
+            counter -= Time.deltaTime / 3;
         else
             holding = false;
 
+        perdeScale.x = counter/max  * startX;
+        perdeScale.y = perde.transform.localScale.y;
+        perdeScale.z = perde.transform.localScale.z;
+
+        perde.transform.localScale = perdeScale;       
         //değer belli bir şeyden yüksekse ya da azsa da buraya yapılabilir
         
     }
