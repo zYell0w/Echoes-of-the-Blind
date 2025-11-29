f85b4472d3ff15764d947a12e4a5745029b23526
Hüseyin <kircihuseyin524@gmail.com>
2025-11-29 18:01:17 +0300

    ses ayar

--- Assets/settings_menu.cs
+++ Assets/settings_menu.cs
@@ -1,47 +1,67 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class settings_menu : MonoBehaviour
 {
     [SerializeField] GameObject entryMenu;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Toggle fullscreenToggle;
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] AudioMixer audioMixer;
+    
+    [Header("Debug")]
+    [SerializeField] Text debugText; // Optional: Assign a UI Text element to see debug messages
+
     void Start()
     {
+        
+      
+        audioMixer.GetFloat("MasterVolume", out float currentVolume);
+
+        volumeLevel = Mathf.Pow(10, currentVolume / 20f);
+        volumeSlider.value = volumeLevel;
+        
+        fullscreenToggle.isOn = Screen.fullScreen;
+        isFullscreen = Screen.fullScreen;
         gameObject.SetActive(false);
+
     }
 
-    // Update is called once per frame
-    void Update()
+    bool isFullscreen = false;
+    float volumeLevel = 0.8f;
+
+    public void OnVolumeSlider(float volume)
     {
-        if(Input.GetKey(KeyCode.Escape))
-            gameObject.SetActive(false);
-
+        volumeLevel = Mathf.Clamp01(volume);
+        
+        
     }
 
+    // Keep your other methods the same...
     public void OnApplyButton()
     {
         gameObject.SetActive(false);
-    }
+        Screen.fullScreen = isFullscreen;
+        float dB = volumeLevel > 0.0001f ? Mathf.Log10(volumeLevel) * 20 : -80f;
+        audioMixer.SetFloat("MasterVolume", dB);
 
-    public void OnResChange()
-    {
         
     }
 
-    public void OnFulscreenToggle()
+    public void OnFulscreenToggle(bool toggle)
     {
-        
-    }
-
-    public void OnVolumeSlider()
-    {
-        
+        isFullscreen = toggle;
     }
 
     void OnDisable()
     {
-        entryMenu.SetActive(true);
+        if (entryMenu != null)
+            entryMenu.SetActive(true);
     }
 
-    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            gameObject.SetActive(false);
+    }
 }
